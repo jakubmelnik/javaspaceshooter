a2ebf69e88f72c95ca07a9f1fa5b0efa3878dff1
jakubmelnik <jakubmelnik@gmail.com>
2025-11-24 23:50:22 +0100

    setup, basic player movement, unfinished enemy movement

---
+++ core/src/main/java/io/jakubmelnik/spaceshooter/Main.java
@@ -0,0 +1,122 @@
+package io.jakubmelnik.spaceshooter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    Texture backgroundTexture;
+    Texture playerTexture;
+    Texture enemyTexture;
+    Texture beamTexture;
+    Texture enemyBeamTexture;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Sprite playerSprite;
+
+    Array<Sprite> enemies;
+
+    @Override
+    public void create() {
+        backgroundTexture = new Texture("background.png");
+        playerTexture = new Texture("playership.png");
+        enemyTexture = new Texture("enemy.png");
+        enemyBeamTexture = new Texture("enemybeam.png");
+        beamTexture = new Texture("beam.png");
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(16, 10);
+
+        playerSprite = new Sprite(playerTexture);
+        playerSprite.setSize(1, 1);
+        playerSprite.setY(1);
+        playerSprite.setX(7);
+
+        enemies = new Array<>();
+        createEnemies();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+        float speed = 9;
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            playerSprite.translateX(speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            playerSprite.translateX(-speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+
+        }
+    }
+
+    private void logic() {
+        float gameWidth = viewport.getWorldWidth();
+        playerSprite.setX(MathUtils.clamp(playerSprite.getX(), 0, gameWidth - playerSprite.getWidth()));
+
+        float delta = Gdx.graphics.getDeltaTime();
+        for (Sprite enemy: enemies){
+            enemy.translateX(2f * delta);
+        }
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float gameWidth = viewport.getWorldWidth();
+        float gameHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, gameWidth, gameHeight);
+        playerSprite.draw(spriteBatch);
+
+        for (Sprite enemy : enemies){
+            enemy.draw(spriteBatch);
+        }
+
+        spriteBatch.end();
+    }
+
+    private void createEnemies(){
+        float enemyWidth = 1, enemyHeight = 1;
+        float gameWidth = viewport.getWorldWidth(), gameHeight = viewport.getWorldHeight();
+
+        Sprite enemy = new Sprite(enemyTexture);
+        enemy.setSize(1,1);
+        enemy.setX(0);
+        enemy.setY(gameHeight-enemyHeight);
+        enemies.add(enemy);
+    }
+
+    private void createBeam(){
+        float playerWidth = playerSprite.getWidth();
+        float playerHeight = playerSprite.getHeight();
+    }
+}
